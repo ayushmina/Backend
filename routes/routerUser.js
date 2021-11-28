5f254f361be09254b577df9b6653fce946c64416
ayushmina <66309981+ayushmina@users.noreply.github.com>
2021-11-28 11:40:22 +0530

    last update for backend

---
+++ routes/routerUser.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const { uploadFile,getData } = require("../controllers/userController");
+const router = express.Router();
+
+const multer = require("multer");
+
+var storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "./uploads");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+
+var uploads = multer({ storage: storage });
+
+ router.get("/",(req,res)=>{
+   res.send("hello");
+ });
+router.post("/upload", uploads.single("xlsx"), uploadFile);
+router.get("/getData",getData);
+
+module.exports = router;
